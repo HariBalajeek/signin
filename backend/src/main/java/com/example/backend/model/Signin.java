0fd12deef808502fb3c82ae38b32eaebdea16c36
Hari Balajee K <haritrance28@gamil.com>
2025-04-19 17:46:14 +0530

    second change in model

--- backend/src/main/java/com/example/backend/model/Signin.java
+++ backend/src/main/java/com/example/backend/model/Signin.java
@@ -1,52 +1,22 @@
 package com.example.backend.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "signin")
+@Data // includes @Getter, @Setter, @ToString, @EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
 public class Signin {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY )
-    private long id;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
     private String email;
     private String password;
-    private String name;
-
-    //getters and setters
-    public long getId() {
-        return id;
-    }
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getEmail(){
-        return email;
-    }
-
-    public void setEmail(String email){
-        this.email = email;
-    }
-    public String getPassword(){
-        return password;
-    }
-    public void setPassword(String password){
-        this.password = password;
-    }
-    public String getName(){
-        return name;
-    }
-
-    public void setName(String name){
-        this.name = name;
-    }
-    public Signin(long id, String email, String password, String name) {
-        this.id = id;
-        this.email = email;
-        this.password = password;
-        this.name = name;
-    }
 }
